d6ed4935f45f6b8fb1935ef4d4cd918bb3a1bfc8
unknown <ethan.russell406@gmail.com>
2022-07-07 15:32:06 -0600

    new file:   src/assets/range.png 	modified:   src/sections/header/Header.js 	modified:   src/sections/header/header.css

--- src/sections/header/Header.js
+++ src/sections/header/Header.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "./header.css"
 import ethan from "../../assets/ethan.png"
-
+import range from "../../assets/range.png"
 
 
 const Header = () => {
@@ -12,11 +12,12 @@ const Header = () => {
             <img className="Ethan" src= {ethan}/>
             <div className="contentbox">
              <h1> Ethan Russell</h1>
-             <h2> Graphic Designer | Web Developer</h2>
-            
+             <h2> Graphic Designer | Web Developer</h2>            
         </div>
         </div>
+        
         </div>
+        <img className="range" src={range} />
         </section>
     )
 
